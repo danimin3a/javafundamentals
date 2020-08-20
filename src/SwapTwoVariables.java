02442811dc9f16d28bb21831f52d21a29819e954
Dan Minea <danimin3a@gmail.com>
2020-08-20 22:31:40 +0300

    Updates

--- src/SwapTwoVariables.java
+++ src/SwapTwoVariables.java
@@ -2,9 +2,8 @@ public class SwapTwoVariables {
     public static void main(String[] args) {
         int a=5;
         int b=3;
-        int c;
         System.out.println("Before swap: a="+a+" b="+b);
-        c=a;
+        int c = a;
         a=b;
         b=c;
         System.out.println("After swap: a="+a+" b="+b);
